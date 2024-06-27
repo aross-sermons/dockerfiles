1d7572247f04159cdfde845a3a7dfeaa2609b2a3
Andrew <aross-sermons@proton.me>
2024-06-27 18:17:03 -0400

    atm9sky

--- atm9sky-24/start.sh
+++ atm9sky-24/start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --name=atm9sky_server --memory=10g -e JAVA_OPTS="-Xms9216m -Xmx9216m" -p 25565:25565 atm9sky
+docker run --name=atm9sky_server --memory=10g -e JAVA_OPTS="-Xms9216m -Xmx9216m" -p 25575:25575 atm9sky
